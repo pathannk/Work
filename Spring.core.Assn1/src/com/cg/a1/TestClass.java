f58ffd5da0e61c75bce4c4a82a8674554ec88fc9
pathannk <101547410+pathannk@users.noreply.github.com>
2022-03-14 12:45:45 +0530

    Add files via upload

---
+++ Spring.core.Assn1/src/com/cg/a1/TestClass.java
@@ -0,0 +1,14 @@
+package com.cg.a1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestClass {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
+		Customer customer1 = applicationContext.getBean("customer1", Customer.class);
+		Customer customer2 = applicationContext.getBean("customer2", Customer.class);
+		customer1.displayCustomerInfo();
+		customer2.displayCustomerInfo();
+	}
+}

39371b72fecdcc1404d58732703d848050f7cde7
pathannk <101547410+pathannk@users.noreply.github.com>
2022-03-14 12:36:36 +0530

    Add files via upload

---
+++ Inheritance/src/com/cg/domain/AdminTest.java
@@ -0,0 +1,14 @@
+package com.cg.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AdminTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
